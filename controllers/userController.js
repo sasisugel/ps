a80e2bb3d899f7464d4a8a37ed2ea56746d6cbd2
Sasikumar S <sasisugel@gmail.com>
2022-04-29 15:58:14 +0530

    error handle if no data

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,14 +14,15 @@ exports.getAllUsers = (req, res) => {
     if (searchStr) userData = users.filter(user => user.name.toLowerCase().includes(searchStr))
     else userData = users
 
-    if (sortByAsc) {
-        userData = userData.sort((user1, user2) => user1.age - user2.age)
-    } else {
-        userData = userData.sort((user1, user2) => user2.age - user1.age)
+    if (userData) {
+        if (sortByAsc) {
+            userData = userData.sort((user1, user2) => user1.age - user2.age)
+        } else {
+            userData = userData.sort((user1, user2) => user2.age - user1.age)
+        }
+        userData = userData.slice(skip, limit)
     }
 
-    userData = userData.slice(skip, limit)
-    console.log(userData)
     res.status(200).json({
         status: 'success',
         results: userData.length,
